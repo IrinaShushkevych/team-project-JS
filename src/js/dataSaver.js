59e310cbeaab21edc606d5b82409f00bc00064bf
ViraKolesnikova <82116461+ViraKolesnikova@users.noreply.github.com>
2021-11-07 23:26:40 +0200

    refactoring

--- src/js/dataSaver.js
+++ src/js/dataSaver.js
@@ -12,4 +12,18 @@ export default class DataSaver {
   // Записать фильм в очередь
   // Удалить фильм из очереди
   // Проверка наличия фильма в очереди
+  constructor() {}
+
+  saveFilmsGenres = async data => {
+    const genresResult = await data;
+    const dataGenres = genresResult.genres;
+    dataGenres.map(obj => (obj.name === 'Science Fiction' ? (obj.name = 'Sci-Fi') : obj.name));
+    localStorage.setItem('genres', JSON.stringify(dataGenres));    
+  };
+
+  getSavedGenres = () => {
+    const savedGenres = localStorage.getItem('genres');
+    const genres = JSON.parse(savedGenres);
+    return genres;
+  };
 }
