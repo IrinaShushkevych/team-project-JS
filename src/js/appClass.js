1a99544e5dca9cbb3c181dc8aa461915a0c41928
IrinaShushkevych <iraolijnyk@gmail.com>
2021-11-07 11:51:02 +0200

    footer css

---
+++ src/js/appClass.js
@@ -0,0 +1,7 @@
+export default class App {
+  //     listener(логотип, кнопки хедера(home, lybrary), инпут, ссылка футера)
+  // Клик логотип и home
+  // Клик lybrary
+  // Клик footer - запуск модалки
+  // input
+}
