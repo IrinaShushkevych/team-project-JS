1b96371e7fb8f1978abc907c2ab8dcbdb74e389d
IrinaShushkevych <iraolijnyk@gmail.com>
2021-11-07 11:28:50 +0200

    11

--- src/js/DataServise.js
+++ src/js/DataServise.js
@@ -1,5 +1,4 @@
-export default
-class APIService {
+export default class APIService {
   constructor() {
     this.keyAPI = 'api_key=a907caf8c46067564d1786718be1cb84';
     this.baseUrl = 'https://api.themoviedb.org/3/';
@@ -7,27 +6,26 @@ class APIService {
     this.url = '';
   }
 
-  fetchData=async(url) => {
+  fetchData = async url => {
     const response = await fetch(url);
-    return response.json()
-  }
-  
+    return response.json();
+  };
+
   getPopularFilms = () => {
     let popularFilms = 'trending/movie/week?';
     this.url = this.baseUrl + popularFilms + this.keyAPI;
-    return this.fetchData(this.url)
-  }
+    return this.fetchData(this.url);
+  };
 
-  getFilmsByQuery = (query) => {
+  getFilmsByQuery = query => {
     let queryEndpoint = `search/movie?query=${query}&`;
-    this.url = this.baseUrl + queryEndpoint + this.keyAPI +`&page=${this.page}`;
-    return this.fetchData(this.url)
-  }
+    this.url = this.baseUrl + queryEndpoint + this.keyAPI + `&page=${this.page}`;
+    return this.fetchData(this.url);
+  };
 
   getFilmsGenres = () => {
     let genresEndpoint = 'genre/movie/list?';
     this.url = this.baseUrl + genresEndpoint + this.keyAPI;
-    return this.fetchData(this.url)
-  }
-
+    return this.fetchData(this.url);
+  };
 }
