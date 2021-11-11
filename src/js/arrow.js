2095a3e3e4ff4876fa1870059d1d907fffb06d6e
IrinaShushkevych <iraolijnyk@gmail.com>
2021-11-11 13:25:37 +0200

    button up and down page

---
+++ src/js/arrow.js
@@ -0,0 +1,59 @@
+import refs from './refs.js';
+import arrowTpl from '../templates/upDownButton.hbs';
+import svgDown from '../images/svg/icons.svg';
+console.log(svgDown);
+
+export default class Arrow {
+  constructor(root = refs.main, header = refs.header, footer = refs.footer) {
+    this.root = root;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  init = () => {
+    this.render();
+    const option = {
+      rootMargin: '0px',
+      threshold: 0.6,
+    };
+    this.observer_header = new IntersectionObserver(this.isScrolling, option);
+    this.observer_footer = new IntersectionObserver(this.isScrolling, option);
+    this.observer_header.observe(document.querySelector('header'));
+    this.observer_footer.observe(document.querySelector('footer'));
+  };
+
+  render = () => {
+    const icon = { up: `.${svgDown}#up`, down: `.${svgDown}#down` };
+    this.root.insertAdjacentHTML('beforeend', arrowTpl(icon));
+    this.up = document.querySelector('.js-button-up');
+    this.down = document.querySelector('.js-button-down');
+  };
+
+  classToggle = elem => {
+    elem.classList.toggle('visually-hidden');
+  };
+
+  isScrolling = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        if (entry.target.classList.contains('header')) {
+          console.log('header');
+          this.classToggle(this.up);
+        }
+        if (entry.target.classList.contains('footer')) {
+          console.log('footer');
+          this.classToggle(this.down);
+        }
+      } else {
+        if (entry.target.classList.contains('header')) {
+          console.log('no header');
+          this.classToggle(this.up);
+        }
+        if (entry.target.classList.contains('footer')) {
+          console.log('no footer');
+          this.classToggle(this.down);
+        }
+      }
+    });
+  };
+}
