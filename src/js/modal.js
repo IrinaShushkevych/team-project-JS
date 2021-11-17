642a7fe91cb7cc98d155ea5777c99a2c123b8386
IrinaShushkevych <iraolijnyk@gmail.com>
2021-11-17 13:50:12 +0200

    team-end

--- src/js/modal.js
+++ src/js/modal.js
@@ -30,9 +30,10 @@ export default class Modal {
     this.refs.btnClose.addEventListener('click', this.onBtnClosePress);
     this.refs.backdrop.addEventListener('click', this.onBackdropClick);
     if (page !== 'team') {
-      this.refs.modalContainer.classList.add('visually-hidden');
+      this.refs.modalContainer.classList.add('hidden');
       this.refs.modalCardRef.classList.remove('hidden');
     } else {
+      this.refs.modalContainer.classList.remove('hidden');
       this.refs.modalCardRef.classList.add('hidden');
       this.refs.memberPhoto = document.querySelector('.killer__image');
       this.refs.memberPhoto.addEventListener('click', this.photoChanging);
